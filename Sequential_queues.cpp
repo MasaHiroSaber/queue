91e58ddb687ff606201978b12f6757b55a11a500
MasaHiroSaber <1171338853@qq.com>
2023-03-19 13:13:14 +0800

    3/19

--- Sequential_queues.cpp
+++ Sequential_queues.cpp
@@ -1,11 +1,61 @@
-#include <iostream>
+#include <stdio.h>
 
 #define MAX_LENGTH 100
 typedef struct Sequential_queues
 {
-    int data[MAX_LENGTH];
-    Sequential_queues *leader;
-    Sequential_queues *tail;
+    int sq_data[MAX_LENGTH];
+    int leader;
+    int tail;
 } sq;
 
-//初始化顺序队列
+void createQueue(sq *p)
+{
+    p->leader = 0;
+    p->tail = 0;
+}
+
+int queueEmpty(sq *p)
+{
+    if(p->leader==p->tail) return 1;
+    else return 0;
+}
+
+
+int queueFull(sq *p)
+{
+    if(p->tail == MAX_LENGTH) return 1;
+    else return 0;
+}
+
+void enterData(sq *p, int data)
+{
+    if(queueFull(p)) return;
+    p->sq_data[p->tail] = data;
+    p->tail++;
+}
+
+void deleteData(sq *p, int *data)
+{
+    if(queueEmpty(p)) return;
+    *data = p->sq_data[p->leader];
+    p->leader++;
+}
+
+void printQueue(sq *p)
+{
+    int i = p->leader;
+    while(i < p->tail)
+    {
+        printf("%d\t",p->sq_data[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    sq sq1;
+    createQueue(&sq1);
+    enterData(&sq1, 1);
+    printQueue(&sq1);
+}
